0ddfe918bda0e91b78d2b5b067d7a975fa27215a
logunkov <logunkovkn@gmail.com>
2023-03-27 13:20:05 +0700

    Add LoginPresenterTests.swift

---
+++ TodoListTests/Scenes/LoginScene/LoginPresenterTests.swift
@@ -0,0 +1,41 @@
+//
+//  LoginPresenterTests.swift
+//  TodoListTests
+//
+//  Created by Constantin on 27.03.2023.
+//
+
+import XCTest
+@testable import TodoList
+
+final class LoginPresenterTests: XCTestCase {
+
+	private let viewController = LoginViewControllerSpy()
+
+	func test_present_withValidRequest_shouldBeRenderSuccess() {
+		let sut = makeSut()
+		let response = LoginModels.Response(success: true)
+
+		sut.present(response: response)
+
+		XCTAssertTrue(viewController.isCalledRender, "Не вызван viewController.render(:)")
+		XCTAssertTrue(viewController.viewModel, "Ошибка в модели данных")
+	}
+
+	func test_present_withFailRequest_shouldBeRenderFailure() {
+		let sut = makeSut()
+		let response = LoginModels.Response(success: false)
+
+		sut.present(response: response)
+
+		XCTAssertTrue(viewController.isCalledRender, "Не вызван viewController.render(:)")
+		XCTAssertFalse(viewController.viewModel, "Ошибка в модели данных")
+	}
+}
+
+// MARK: - Private
+private extension LoginPresenterTests {
+	func makeSut() -> LoginPresenter {
+		LoginPresenter(viewController: viewController)
+	}
+}
