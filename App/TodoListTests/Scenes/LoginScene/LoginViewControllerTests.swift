bad77e2e26f973dde347484824804ab7f8a2997c
logunkov <logunkovkn@gmail.com>
2023-04-12 17:46:04 +0700

    Update LoginViewControllerTests.swift

--- App/TodoListTests/Scenes/LoginScene/LoginViewControllerTests.swift
+++ App/TodoListTests/Scenes/LoginScene/LoginViewControllerTests.swift
@@ -13,38 +13,25 @@ final class LoginViewControllerTests: XCTestCase {
 	// MARK: - Private Properties
 
 	private var interactor: LoginInteractorSpy! // swiftlint:disable:this implicitly_unwrapped_optional
-	private var window: UIWindow! // swiftlint:disable:this implicitly_unwrapped_optional
-	private var sut: LoginViewController! // swiftlint:disable:this implicitly_unwrapped_optional
 
-	// MARK: - Lifecycle
-
-	override func setUp() {
-		super.setUp()
-
-		let storyboard = UIStoryboard(name: "LoginScene", bundle: Bundle.main)
-		sut = storyboard.instantiateViewController(identifier: "LoginScene") as? LoginViewController
-		interactor = LoginInteractorSpy()
-		sut.interactor = interactor
-		window = UIWindow()
-		window.addSubview(sut.view)
-		RunLoop.current.run(until: Date())
-	}
-
-	override func tearDown() {
-		sut = nil
-		interactor = nil
-		window = nil
-
-		super.tearDown()
-	}
-
-	// MARK: - Internal Methods
+	// MARK: - Public Methods
 
 	func test_render_withValidRequest_shouldLoggedIn() {
-		sut.loginText = "validEmail"
-		sut.passText = "validPass"
+		let sut = makeSut()
 		sut.login()
 
 		XCTAssertTrue(interactor.isCalledLogin, "Не вызван interactor.login(:)")
+	}
+}
+
+// MARK: - Private Methods
+
+private extension LoginViewControllerTests {
+	func makeSut() -> LoginViewController {
+		let mainView = LoginViewSpy()
+		let viewController = LoginViewController(mainView: mainView)
+		interactor = LoginInteractorSpy()
+		viewController.interactor = interactor
+		return viewController
 	}
 }
